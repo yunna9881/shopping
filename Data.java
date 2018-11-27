750be15f59463c99ae0e405c0e5b3341a65e15a2
Yunna Won <41393464+yunna9881@users.noreply.github.com>
2018-11-26 22:03:41 -0500

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,51 @@
+package assignment3;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Data
+ */
+@WebServlet("/Data")
+public class Data extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Data() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+		
+		LoginNamePrint info = new LoginNamePrint();
+		info.setId(id);
+		info.setPasswd(passwd);
+		request.setAttribute("information", info);
+
+		RequestDispatcher rd = request.getRequestDispatcher("Welcome.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ LoginNamePrint.java
@@ -0,0 +1,28 @@
+package assignment3;
+
+public class LoginNamePrint
+{
+	private String id, passwd;
+	
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+	
+	public String getId()
+	{
+		return id;
+	}
+	
+	public void setPasswd(String passwd)
+	{
+		this.passwd=passwd;
+	}
+	
+	public String getPasswd()
+	{
+		return passwd;
+	}
+	
+	
+}
